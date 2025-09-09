666163caed576bab7ecfd03b51b4ce26b3f4ee64
AlexeyTarasov77 <alexxey2707@gmail.com>
2025-09-09 11:44:59 +0200

    eliminate zod usage for env validation

--- src/shared/config/env.ts
+++ src/shared/config/env.ts
@@ -1,20 +1,20 @@
-import { z } from "zod"
-
-const appConfigSchema = z.object({
-  NEXT_PUBLIC_TENDERLY_NETWORK_URL: z.string().url(),
-  NEXT_PUBLIC_REOWNAPPKIT_PROJECT_ID: z.string(),
-  NEXT_PUBLIC_SERVER_URL: z.string().url()
-})
-
-
-// INFO: for client side config variables should be explicitly specified due to how next.js inlines env variables in client bundle
-export const env = appConfigSchema.parse(
-  typeof window === undefined ? process.env :
-    {
-      NEXT_PUBLIC_TENDERLY_NETWORK_URL: process.env.NEXT_PUBLIC_TENDERLY_NETWORK_URL,
-      NEXT_PUBLIC_REOWNAPPKIT_PROJECT_ID: process.env.NEXT_PUBLIC_REOWNAPPKIT_PROJECT_ID,
-      NEXT_PUBLIC_SERVER_URL: process.env.NEXT_PUBLIC_SERVER_URL,
+function parseRequiredEnv() {
+  const env = {
+    NEXT_PUBLIC_TENDERLY_NETWORK_URL: process.env.NEXT_PUBLIC_TENDERLY_NETWORK_URL!,
+    NEXT_PUBLIC_REOWNAPPKIT_PROJECT_ID: process.env.NEXT_PUBLIC_REOWNAPPKIT_PROJECT_ID!,
+    NEXT_PUBLIC_SERVER_URL: process.env.NEXT_PUBLIC_SERVER_URL!,
+  }
+  Object.entries(env).forEach(([k, v]) => {
+    const isServerRuntime = typeof window === "undefined"
+    // INFO: variables not prefixed with NEXT_PUBLIC would be empty on frontend
+    if (!isServerRuntime && k.startsWith("NEXT_PUBLIC_")) {
+      return
     }
-)
+    if (!v || !v.trim()) {
+      throw new Error(`Error validating environment: '${k}' is required`)
+    }
+  })
+  return env
+}
 
-
+export const env = parseRequiredEnv()
